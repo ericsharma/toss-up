8e664943242183a325e3b3ecb133f32abf12542d
ericsharma <sharma.e@husky.neu.edu>
2025-03-27 15:25:50 -0400

    Funk's idea

--- projects/toss-up/contracts/TossUp.algo.ts
+++ projects/toss-up/contracts/TossUp.algo.ts
@@ -16,32 +16,13 @@ export class TossUp extends Contract {
     return this.owner.value;
   }
 
-  private getRandom(player: Address): uint64 {
-    let seed: uint64;
-
-    const blockSeed =
-      extractUint64(blocks[this.txn.firstValid - 1].seed, blocks[this.txn.firstValid - 1].timestamp % 24) % 24;
-
+  private getRandom(): uint64 {
     const proposer = blocks[this.txn.firstValid - 1].proposer;
+    const blockSeed = blocks[this.txn.firstValid - 1].seed;
 
-    // unfortunatley we cannot use proposer.lastHeartBeat as it is not available in the smart contract without passing proposer as a reference which we don't want to do
-    // const heartBeatSeed = proposer.lastHeartbeat % 24; // mod 24 since thats the last valid starting index for extractUint64()
-
-    // blocks[globals.round - 2]. // update the proposer's lastHeartBeat to the current round
-    const maxSeed = 4294967295; // maximum uint that will allow for maxSeed * maxSeed without causing an overflow
-
-    const playerSeed = extractUint64(player, blockSeed) % maxSeed; // introduce something unique to the player so the random seed is different for games within the same round
-
-    const ownerSeed = extractUint64(this.owner.value, blockSeed) % maxSeed; // introduce something unique to owner so the random seed is different for games from the same player within the same round
-
-    const intraRoundRandomness = (playerSeed * ownerSeed) % 24;
-
-    if (proposer === globals.zeroAddress) {
-      // if proposer is zeroAddress then we're in localnet so choose a different dynamic seed
-      seed = extractUint64(player, intraRoundRandomness);
-    } else {
-      seed = extractUint64(proposer, intraRoundRandomness);
-    }
+    const seed = getbyte(sha256(concat(proposer, blockSeed)), 0);
+    // My interpretation of Funk's suggestion: https://discord.com/channels/491256308461207573/1352324685207175269/1354835871152607453
+    // One downside of this approach is that the seed will be the same for all transactions in the same block
 
     return seed;
   }
@@ -50,7 +31,7 @@ export class TossUp extends Contract {
   play(payment: PayTxn, player: Address): uint64 {
     asserts(player !== this.owner.value, this.amount.value === payment.amount, this.txn.sender === this.app.creator);
 
-    const seed = this.getRandom(player);
+    const seed = this.getRandom();
 
     if (seed % 2 === 0) {
       sendPayment({ receiver: this.owner.value, amount: this.amount.value + payment.amount });
